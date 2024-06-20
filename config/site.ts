8e90ad8ce0e60ac9b378eca913fb75d9f5f13d4e
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-06-20 12:40:43 +0530

    feat(config): init styling and types

---
+++ config/site.ts
@@ -0,0 +1,48 @@
+import { NavItem } from '@/types';
+
+import { Metadata } from 'next';
+
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  metadata: {
+    title: {
+      default: "oh gosh i'm gonna...",
+      template: '%s | coomer.org',
+    },
+    icons: {
+      icon: '/favicon.png',
+    },
+    authors: [{ name: 'anon', url: 'https://peacefulfoundation.org/' }],
+  } as Metadata,
+  name: 'coomer.org',
+  description: 'curated coomer memes',
+  headerLinks: [
+    {
+      name: 'not an nft',
+      url: '/not-an-nft',
+    },
+    {
+      name: 'ethos',
+      url: '/ethos',
+    },
+  ] as NavItem[],
+  footerLinks: {
+    left: [
+      {
+        name: 'struggling? check out easypeasy',
+        url: 'https://easypeasymethod.org',
+      },
+    ] as NavItem[],
+    right: [
+      {
+        name: 'quiteasily',
+        url: 'https://quiteasily.org/',
+      },
+      {
+        name: 'peaceful',
+        url: 'https://peacefulfoundation.org/',
+      },
+    ] as NavItem[],
+  },
+};
---
+++ types/index.ts
@@ -0,0 +1,4 @@
+export type NavItem = {
+  name: string;
+  url: string;
+};
