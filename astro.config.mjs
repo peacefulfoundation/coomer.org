b604459345024dc0b4e689541c0a21b110f806d3
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-15 12:29:56 +0530

    feat(deploy): add gitlab ci and config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://coomer.org',
+  outDir: 'public',
+  publicDir: 'static',
   integrations: [tailwind()],
 });
