0fe363f1ae64547e4c17289be2c826acf872009b
Abhigyan Trips <contact@abhigyantrips.dev>
2024-10-30 23:47:34 +0530

    fix(components): change anchors to Links

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,28 +1,30 @@
 import { siteConfig } from '@/config/site';
 
+import Link from 'next/link';
+
 export function SiteFooter() {
   return (
     <footer className="center container flex h-20 w-full items-center justify-between">
       <div className="text-md flex space-x-2 font-semibold">
         {siteConfig.footerLinks.left.map((item, index) => (
-          <a
+          <Link
             key={index}
             className="p-2 underline-offset-4 hover:underline"
             href={item.href}
           >
             {item.title}
-          </a>
+          </Link>
         ))}
       </div>
       <div className="text-md flex space-x-2 font-semibold">
         {siteConfig.footerLinks.right.map((item, index) => (
-          <a
+          <Link
             key={index}
             className="p-2 underline-offset-4 hover:underline"
             href={item.href}
           >
             {item.title}
-          </a>
+          </Link>
         ))}
       </div>
     </footer>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,18 +1,30 @@
 import { siteConfig } from '@/config/site';
 
+import Image from 'next/image';
+import Link from 'next/link';
+
 export function SiteHeader() {
   return (
     <header className="center container flex h-20 w-full items-center justify-between">
-      <h1 className="block p-2 text-2xl font-bold">{siteConfig.title}</h1>
+      <Image
+        src="/favicon.png"
+        height={32}
+        width={32}
+        alt={siteConfig.title}
+        className="block rounded-full md:hidden"
+      />
+      <h1 className="hidden p-2 text-2xl font-bold md:block">
+        {siteConfig.title}
+      </h1>
       <div className="flex space-x-2 font-semibold">
         {siteConfig.headerLinks.map((item, index) => (
-          <a
+          <Link
             key={index}
             className="p-2 underline-offset-4 hover:underline"
             href={item.href}
           >
             {item.title}
-          </a>
+          </Link>
         ))}
       </div>
     </header>
