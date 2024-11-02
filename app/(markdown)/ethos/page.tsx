0c0452a1061dab89b87154becd9254d6fae4e3bb
Abhigyan Trips <contact@abhigyantrips.dev>
2024-11-02 08:48:11 +0530

    chore(ui): edit metadata

--- app/(markdown)/ethos/page.tsx
+++ app/(markdown)/ethos/page.tsx
@@ -1,5 +1,12 @@
 import Markdown from '@/markdown/ethos.mdx';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ethos',
+  description: 'the ethos of the project.',
+};
+
 export default function Ethos() {
   return <Markdown />;
 }
--- app/(markdown)/not-an-nft/page.tsx
+++ app/(markdown)/not-an-nft/page.tsx
@@ -1,5 +1,12 @@
 import Markdown from '@/markdown/not-an-nft.mdx';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'not an nft',
+  description: 'nfts: no frickin thanks',
+};
+
 export default function NotAnNFT() {
   return <Markdown />;
 }
