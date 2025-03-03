95ee6165e0cbc5db6e27be217c5287188992a215
Abhigyan Trips <contact@abhigyantrips.dev>
2025-03-03 21:39:52 +0530

    fix(ui): 'start over' button

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,10 @@
 import { siteConfig } from '@/config/site';
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 import PostList from '@/components/post-list';
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.40))] w-full flex-col items-center justify-center space-y-4">
       <div className="grid grid-flow-row items-center gap-4 md:grid-flow-col">
@@ -62,7 +59,7 @@ export default function Home() {
         <button
           onClick={() => {
             localStorage.removeItem('lastCursor');
-            router.refresh();
+            window.location.reload();
           }}
           className="w-fit items-center rounded-full border-[1.5px] border-black bg-black px-4 py-2.5 font-semibold text-white transition-colors duration-200 ease-in-out hover:bg-white hover:text-black"
         >
